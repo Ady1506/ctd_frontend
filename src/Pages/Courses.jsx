3fab44b2b82fdd3b03044eefcb70638a37fd92aa
Ady1506 <aaadya_be22@thapar.edu>
2025-04-06 15:46:17 +0530

    login

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -7,7 +7,7 @@ const Courses = () => {
     const [searchTerm, setSearchTerm] = useState('');
 
     return (
-        <div className='body flex flex-col w-full h-full p-4 md:p-10 bg-gray-100'>
+        <div className='body flex flex-col w-full h-full p-4 md:p-10 md:pb-0 bg-gray-100'>
             {/* Header */}
             <div className='flex flex-row justify-between items-center w-full'>
                 <h1 className='text-2xl md:text-3xl font-semibold text-dblue'>Courses</h1>
@@ -15,7 +15,7 @@ const Courses = () => {
             </div>
 
             {/* Toggle Buttons & Search */}
-            <div className='sub-body flex gap-3 mt-4 md:mt-4 p-2 items-center w-full'>
+            <div className='sub-body flex gap-3 p-2 pt-4 md:pt-4 items-center w-full'>
                 <button 
                     onClick={() => setSelectedTab('all')}
                     className={`px-4 py-2 rounded text-sm font-semibold ${selectedTab === 'all' ? 'bg-[#173061] text-white' : 'bg-[#626469] text-white'}`}>
